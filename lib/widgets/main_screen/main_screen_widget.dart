4205b505b1355139e92d5ee2014c5e2f0fa69056
THE-STANLEY <offstanley56@gmail.com>
2024-11-01 20:38:13 +0500

    added bottom app navigation

---
+++ lib/widgets/main_screen/main_screen_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class MainScreenWidget extends StatefulWidget {
+  const MainScreenWidget({super.key});
+
+  @override
+  State<MainScreenWidget> createState() => _MainScreenWidgetState();
+}
+
+class _MainScreenWidgetState extends State<MainScreenWidget> {
+  int _selectedTab = 0;
+
+  static const List<Widget> _widgetOptions = <Widget> [
+    Text(
+      'Home'
+    ),
+    Text(
+      'Movies'
+    ),
+    Text(
+      'Series'
+    ),
+  ];
+
+  void onSelectTab(int index) {
+    if (_selectedTab == index) return;
+    setState(() {
+      _selectedTab = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TMDB', style: TextStyle(color: Colors.white))
+      ),
+      body: Center(
+        child: _widgetOptions[_selectedTab],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'Movies',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.tv_sharp),
+            label: 'Series',
+          ),
+        ],
+        onTap: onSelectTab,
+      ),
+    );
+  }
+}
